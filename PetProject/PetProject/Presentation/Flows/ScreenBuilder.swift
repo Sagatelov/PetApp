adbad460aefedba1c5f217c542c51a42742af175
Sagatelov <sagatelov@gmail.com>
2024-01-19 18:53:30 +0300

    add func showEditUserController in ScreenBuilder, func build editing user's screen

--- PetProject/PetProject/Presentation/Flows/ScreenBuilder.swift
+++ PetProject/PetProject/Presentation/Flows/ScreenBuilder.swift
@@ -10,11 +10,22 @@ import Foundation
 
 protocol ScreenBuilderProtocol {
     func initialUserController(coordinator: CoordinatorConfigProtocol) -> UsersViewController
+    func showEditUserController(user: UsersModel, coordinator:CoordinatorConfigProtocol ) -> UserEditViewController
     func postsController(coordinator: CoordinatorConfigProtocol) -> UsersViewController
     func commentsContrller(coordinator: CoordinatorConfigProtocol) -> UsersViewController
 }
 
 final class ScreenBuilder: ScreenBuilderProtocol {
+    
+    func showEditUserController(user: UsersModel, coordinator:CoordinatorConfigProtocol ) -> UserEditViewController {
+        let network = NetworkService()
+        let coreDataStorage = CoreDataStorage.sared
+        let dataManager = DataManager(network: network, coreData: coreDataStorage)
+        let viewModel = UserEditViewModel(user: user, dataManager: dataManager, flowCoordinator: coordinator)
+        let editController = UserEditViewController.initUserEdit(viewModel: viewModel)
+        return editController
+    }
+    
     func postsController(coordinator: CoordinatorConfigProtocol) -> UsersViewController {
 
         return UsersViewController()
