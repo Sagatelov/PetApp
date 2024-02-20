c211978456b7e662b89bd72f34c9e50288bc388b
Sagatelov <sagatelov@gmail.com>
2024-02-21 00:58:32 +0300

    add registration module

--- PetProject/PetProject/Services/AuthManager/AuthManager.swift
+++ PetProject/PetProject/Services/AuthManager/AuthManager.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import FirebaseAuth
 
-class AuthManager {
+protocol AuthManagerProtocol {
+    func createUser(email: String, password: String, completion: @escaping (_ success: Bool) -> Void)
+    func signIn(email: String, pass: String, completion: @escaping (_ success: Bool) -> Void)
+}
 
+class AuthManager: AuthManagerProtocol {
+    func createUser(email: String, password: String, completion: @escaping (_ success: Bool) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) {(authResult, error) in
+            if let user = authResult?.user {
+                print(user)
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+    
+    func signIn(email: String, pass: String, completion: @escaping (_ success: Bool) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: pass) { (result, error) in
+            if let error = error, let _ = AuthErrorCode.Code(rawValue: error._code) {
+                completion(false)
+            } else {
+                completion(true)
+            }
+        }
+    }
+    
 }
