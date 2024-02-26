df22e48af4de3dcfac05c8951092c7ae55e8c033
Sagatelov <sagatelov@gmail.com>
2024-02-26 12:01:43 +0300

    add registration and profile init function, did refactoring of ScreenBuilderProtocol in ScreenBuilder

--- PetProject/PetProject/Presentation/Flows/ScreenBuilder.swift
+++ PetProject/PetProject/Presentation/Flows/ScreenBuilder.swift
@@ -9,47 +9,86 @@ import Foundation
 
 
 protocol ScreenBuilderProtocol {
-    func initialUserList(coordinator: CoordinatorConfigProtocol) -> UsersViewController
-    func showEditUserScreen(user: UsersModel, coordinator:CoordinatorConfigProtocol ) -> UserEditViewController
-    func postsList(coordinator: CoordinatorConfigProtocol, userId: Int) -> PostsListViewController
-    func commentsList(coordinator: CoordinatorConfigProtocol, postsId: Int) -> CommentsListViewController
+    
+    //MARK: Registration Flow
+    func signUpScreen(coordinator: RegistrationCoordinatorConfig) -> SignUpViewController
+    func loginScreen(coordinator:  RegistrationCoordinatorConfig) -> LoginViewController
+    func welcomeScreen(coordinator: RegistrationCoordinatorConfig) -> WelcomeViewController
+    
+    //MARK: User Flow
+    func usersScreen(coordinator: UserCoordinatorConfig) -> UsersViewController
+    func editUserScreen(user: UsersModel, coordinator:UserCoordinatorConfig ) -> UserEditViewController
+    func postsList(coordinator: UserCoordinatorConfig, userId: Int) -> PostsListViewController
+    func commentsList(coordinator: UserCoordinatorConfig, postsId: Int) -> CommentsListViewController
+    
+    //MARK: Profile Flow
+    func profileScreen(coordinator: ProfileCoordinatorConfig) -> ProfileViewController
+    
+    //MARK: Services init
+    init(network: NetworkServiceProtocol, coreDataStorage: CoreDataStorage, authManager: AuthManagerProtocol)
 }
 
 final class ScreenBuilder: ScreenBuilderProtocol {
+    var dataManager: DataManagerProtocol
+    var network: NetworkServiceProtocol
+    var coreDataStorage: CoreDataStorage
+    var authManager: AuthManagerProtocol
     
-    func showEditUserScreen(user: UsersModel, coordinator:CoordinatorConfigProtocol ) -> UserEditViewController {
-        let network = NetworkService()
-        let coreDataStorage = CoreDataStorage.sared
-        let dataManager = DataManager(network: network, coreData: coreDataStorage)
+    init(network: NetworkServiceProtocol, coreDataStorage: CoreDataStorage, authManager: AuthManagerProtocol) {
+        self.network = network
+        self.coreDataStorage = coreDataStorage
+        self.authManager = authManager
+        self.dataManager = DataManager(network: network, coreData: coreDataStorage)
+    }
+    
+    //MARK: - Registration Flow
+    func welcomeScreen(coordinator: RegistrationCoordinatorConfig) -> WelcomeViewController {
+        let viewModel = WelcomeViewModel(dataManager: dataManager, flowCoordinator: coordinator)
+        let screen = WelcomeViewController.initWelcomeScreen(viewModel: viewModel)
+        return screen
+    }
+    
+    func signUpScreen(coordinator: RegistrationCoordinatorConfig) -> SignUpViewController {
+        let viewModel = SignUpViewModel(dataManager: dataManager, flowCoordinator: coordinator, authManager: authManager)
+        let screen = SignUpViewController.initSignScreen(viewModel: viewModel)
+        return screen
+    }
+    
+    func loginScreen(coordinator:  RegistrationCoordinatorConfig) -> LoginViewController {
+        let viewModel = LoginViewModel(dataManager: dataManager, flowCoordinator: coordinator, authManager: authManager)
+        let screen = LoginViewController.initLoginScreen(viewModel: viewModel)
+        return screen
+    }
+    
+    //MARK: - User Flow
+    func editUserScreen(user: UsersModel, coordinator: UserCoordinatorConfig ) -> UserEditViewController {
         let viewModel = UserEditViewModel(user: user, dataManager: dataManager, flowCoordinator: coordinator)
         let editController = UserEditViewController.initUserEdit(viewModel: viewModel)
         return editController
     }
     
-    func postsList(coordinator: CoordinatorConfigProtocol, userId: Int) -> PostsListViewController {
-        let network = NetworkService()
-        let coreDataStorage = CoreDataStorage.sared
-        let dataManager = DataManager(network: network, coreData: coreDataStorage)
+    func postsList(coordinator: UserCoordinatorConfig, userId: Int) -> PostsListViewController {
         let viewModel = PostsListViewModel(userId: userId, dataManager: dataManager, flowCoordinator: coordinator)
         let userListsInit = PostsListViewController.initPostsList(viewModel: viewModel)
         return userListsInit
     }
     
-    func commentsList(coordinator: CoordinatorConfigProtocol, postsId: Int) -> CommentsListViewController {
-        let network = NetworkService()
-        let coreDataStorage = CoreDataStorage.sared
-        let dataManager = DataManager(network: network, coreData: coreDataStorage)
+    func commentsList(coordinator: UserCoordinatorConfig, postsId: Int) -> CommentsListViewController {
         let viewModel = CommentsListViewModel(postId: postsId, dataManager: dataManager, flowCoordinator: coordinator)
         let initCommentsList = CommentsListViewController.initCommentsList(viewModel: viewModel)
         return initCommentsList
     }
     
-    func initialUserList(coordinator: CoordinatorConfigProtocol) -> UsersViewController {
-        let network = NetworkService()
-        let coreDataStorage = CoreDataStorage.sared
-        let dataManager = DataManager(network: network, coreData: coreDataStorage)
+    func usersScreen(coordinator: UserCoordinatorConfig) -> UsersViewController {
         let viewModel = UsersViewModel(dataManager: dataManager, flowCoordinator: coordinator)
         let usersController = UsersViewController.initUsersList(viewModel: viewModel)
         return usersController
+    }
+    
+    //MARK: - Profile Flow
+    func profileScreen(coordinator: ProfileCoordinatorConfig) -> ProfileViewController {
+        let viewModel = ProfileViewModel(dataManager: dataManager, flowCoordinator: coordinator)
+        let controller = ProfileViewController.initProfileController(viewModel: viewModel)
+        return controller
     }
 }
