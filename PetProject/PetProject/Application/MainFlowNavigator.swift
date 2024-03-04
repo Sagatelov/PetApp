532b862b90656662b9714c907487cb9fde2ff317
Sagatelov <sagatelov@gmail.com>
2024-03-05 00:21:44 +0300

    add init logic in MainFlowNavigator

--- PetProject/PetProject/Application/MainFlowNavigator.swift
+++ PetProject/PetProject/Application/MainFlowNavigator.swift
@@ -8,16 +8,16 @@
 import Foundation
 import UIKit
 
-protocol InitialVCForFlowNavigatorProtocol{
+protocol InitialVCForFlowNavigatorProtocol {
     func initController() -> UIViewController
 }
 
 protocol MainFlowNavigatorProtocol {
     init(window: UIWindow)
     var tabBar: UITabBarController { get }
-    var navigators: [CoordinatorConfigProtocol] { get }
+    var navigators: [InitialVCForFlowNavigatorProtocol] { get }
+    var registration: InitialVCForFlowNavigatorProtocol { get }
     var screenBuilder: ScreenBuilderProtocol { get }
-    
 }
 
 final class MainFlowNavigator: MainFlowNavigatorProtocol {
@@ -25,21 +25,43 @@ final class MainFlowNavigator: MainFlowNavigatorProtocol {
     
     private var window: UIWindow
     var tabBar: UITabBarController
-    var navigators: [CoordinatorConfigProtocol] = []
+    var navigators: [InitialVCForFlowNavigatorProtocol]
+    var registration: InitialVCForFlowNavigatorProtocol
     var screenBuilder: ScreenBuilderProtocol
     
-    
+    //MARK: services
+    var network: NetworkServiceProtocol
+    var coreDataStorage: CoreDataStorage
+    var authManager: AuthManagerProtocol
     
     required init(window: UIWindow) {
-        screenBuilder = ScreenBuilder()
-        navigators = [UsersFlowCoordinator(screenBuilder: screenBuilder)]
+        //init services
+        network = NetworkService()
+        coreDataStorage = CoreDataStorage.sared
+        authManager = AuthManager()
         
+        //init builder screen
+        screenBuilder = ScreenBuilder(network: network,
+                                      coreDataStorage: coreDataStorage,
+                                      authManager: authManager)
+        //Coordinators
+        navigators = [UsersFlowCoordinator(screenBuilder: screenBuilder),
+                      ProfileFlowCoordinator(screenBuilder: screenBuilder)]
+        
+        registration = RegFlowCoordinator(screenBuilder: screenBuilder)
+        
+        //MARK: Root controllers
         let controllers = navigators.map { $0.initController() }
+        let registration = registration.initController()
+        registration.modalPresentationStyle = .fullScreen
+        registration.modalTransitionStyle = .crossDissolve
+        
         tabBar = UITabBarController()
         tabBar.setViewControllers(controllers, animated: true)
         
         self.window = window
         window.rootViewController = tabBar
+        tabBar.present(registration, animated: false)
     }
 }
 
